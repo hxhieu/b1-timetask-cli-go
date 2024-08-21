f5a684bbd225dfecc19933b68af9a9b9ae36c983
Hugh Hoang <hugh.hoang@gmail.com>
2024-08-21 22:26:06 +1200

    chore: tweak test run script output

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,6 +6,7 @@ coverageReport='.coverage/index.html'
 
 # Run test and generate coverage output an report
 go test -v -coverprofile "$coverageOut" ./...
+echo "=== RUN   Coverage threshold"
 go tool cover -html "$coverageOut" -o "$coverageReport"
 
 # Coverage threshold check, this can be set in env var
@@ -13,8 +14,10 @@ coverageThreshold="${COVERAGE_THRESHOLD:=70.0}"
 totalCoverage=$(go tool cover -func="$coverageOut" | grep total | grep -Eo '[0-9]+\.[0-9]+')
 
 if (( $(echo "$totalCoverage $coverageThreshold" | awk '{print ($1 < $2)}') )); then
-    echo '---'
-    echo "Tests passed but total coverage failed at $totalCoverage%, out of $coverageThreshold%" >&2
-    echo '---'
+    echo "--- FAIL: Coverage at $totalCoverage%, out of $coverageThreshold%" >&2
+    echo "FAIL" >&2
     exit 1
+else
+    echo "--- PASS: Coverage at $totalCoverage%, out of $coverageThreshold%"
+    echo "PASS"
 fi
