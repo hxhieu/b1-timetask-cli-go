c24e727bc62a52c4e08d109d9764e0d20dadb194
Hugh Hoang <hugh.hoang@gmail.com>
2024-08-21 19:30:23 +1200

    chore: binary build

--- main.go
+++ main.go
@@ -9,8 +9,12 @@ func main() {
 	cli := cmd.CLI{}
 	ctx := kong.Parse(
 		&cli,
-		kong.Name("b1-timetask-cli"),
+		kong.Name("/path/to/the/cli"),
 		kong.Description("A CLI tool to semi-automate the creation of time task."),
+		kong.ConfigureHelp(kong.HelpOptions{
+			Compact:      true,
+			NoAppSummary: true,
+		}),
 	)
 	err := ctx.Run(cmd.CLIContext{Debug: cli.Debug})
 	ctx.FatalIfErrorf(err)
