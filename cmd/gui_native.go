010ce548b04de7d155cab81dd32ead7e55ccaefd
Hugh Hoang <hugh.hoang@gmail.com>
2024-08-26 11:40:02 +1200

    feat: experimenting `https://fyne.io/` as Native GUI solution

--- cmd/gui_native.go
+++ cmd/gui_native.go
@@ -3,6 +3,10 @@
 package cmd
 
 import (
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/container"
+	"fyne.io/fyne/widget"
 	"github.com/hxhieu/b1-timetask-cli-go/console"
 )
 
@@ -13,6 +17,20 @@ func (c *guiCmd) Run(ctx CLIContext) error {
 	}
 
 	// DEBUG MODE ONLY
+
+	a := app.New()
+	w := a.NewWindow("B1 TimeTask CLI GUI")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+	w.Resize(fyne.NewSize(1024, 768))
+
+	w.ShowAndRun()
 
 	return nil
 }
