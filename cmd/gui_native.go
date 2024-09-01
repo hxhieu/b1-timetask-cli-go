b0fab4cdf48255006e45b40dab8cf818f44470a5
Hugh Hoang <hugh.hoang@gmail.com>
2024-09-01 17:44:57 +1200

    refactor: move Fyne related to fyne module

--- cmd/gui_native.go
+++ cmd/gui_native.go
@@ -3,28 +3,10 @@
 package cmd
 
 import (
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
-	"github.com/hxhieu/b1-timetask-cli-go/gui/fyne/ui"
+	mygui "github.com/hxhieu/b1-timetask-cli-go/gui/fyne"
 )
 
-// Initial state
-var appState *ui.AppState
-
 func (c *guiCmd) Run(ctx CLIContext) error {
-	appState = ui.NewAppState()
-	w := appState.App.NewWindow(APP_TITLE)
-	w.SetMaster()
-	w.CenterOnScreen()
-	w.Resize(fyne.NewSize(1024, 768))
-	defer w.ShowAndRun()
-	appState.MainWindow = w
-
-	toolbar := ui.NewMenu(appState)
-
-	content := container.NewBorder(toolbar, nil, nil, nil, widget.NewLabel("Content"))
-
-	w.SetContent(content)
+	mygui.NewNativeGui(APP_TITLE)
 	return nil
 }
