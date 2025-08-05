c2de40f395ad01b1de10a60ea895daaba455ca57
Hugh Hoang <hugh.hoang@gmail.com>
2025-08-05 23:28:15 +1200

    wip: parse event subject for task code and desc

--- cmd/time_create.go
+++ cmd/time_create.go
@@ -1,9 +1,7 @@
 package cmd
 
 import (
-	"errors"
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/hxhieu/b1-timetask-cli-go/common"
@@ -17,11 +15,11 @@ type createTimePrepResult struct {
 	tasks  []*common.TimeTaskInput
 }
 
-func createTimePrepSteps(ctx CLIContext, inputFile *string) (*createTimePrepResult, *intervals_api.Client, error) {
+func createTimePrepSteps(ctx CLIContext, inputFile *string, weekOffset int, subjectTemplate *string) (*createTimePrepResult, *intervals_api.Client, error) {
 	// Shared vars between steps
 	result := &createTimePrepResult{}
 	var client *intervals_api.Client
-	var taskParser *common.TaskCsvParser
+	// var taskParser *common.TaskCsvParser
 
 	// instantiate a Progress Writer and set up the options
 	pw := progress.NewWriter()
@@ -50,90 +48,96 @@ func createTimePrepSteps(ctx CLIContext, inputFile *string) (*createTimePrepResu
 
 	// Parse and fetch tasks from CSV
 	if !job.IsErrored() {
-		job = newJobTrack(pw, "Check task inputs", ctx)
+		// job = newJobTrack(pw, "Check task inputs", ctx)
 
 		// Concat IDs, to pass to the remoter server
-		var tasks string
-		var projects string
+		// var tasks string
+		// var projects string
 
-		if parser, err := common.NewTaskParser(inputFile); err == nil {
-			for _, t := range parser.Tasks {
-				if t != nil {
-					tasks += t.Task + ","
-				}
-			}
-			tasks = strings.TrimSuffix(tasks, ",")
-
-			// Fetch needed details from remote
-
-			if remoteTasks, err := client.FetchTasks(tasks); err == nil {
-				// TODO: Optimise this? nested loops here
-				for _, remoteTask := range *remoteTasks {
-					// Also build the project IDs list
-					projects += remoteTask.ProjectId + ","
-					for _, localTask := range parser.Tasks {
-						if localTask != nil && localTask.Task == remoteTask.LocalId {
-							localTask.ProjectId = remoteTask.ProjectId
-							localTask.Id = remoteTask.Id
-							localTask.Title = remoteTask.Title
-							// Truncate long title
-							if len(localTask.Title) > 50 {
-								localTask.Title = localTask.Title[:50]
-							}
-						}
-					}
-				}
-				projects = strings.TrimSuffix(projects, ",")
-
-				// Fetch work types, because they are setup per project
-				if remoteWorkTypes, err := client.FetchProjectWorkTypes(projects); err == nil {
-					// TODO: Optimise this? nested loops here
-					for _, localTask := range parser.Tasks {
-						var defaultWorkType *string
-						// Walk all work types of the same project
-						for _, remoteWorkType := range *remoteWorkTypes {
-							if localTask != nil && localTask.ProjectId == remoteWorkType.ProjectId {
-								localTask.WorkTypeId = remoteWorkType.WorkTypeId
-								defaultWorkType = &remoteWorkType.WorkType
-								// Finally found the match by work type name
-								if localTask.WorkType == remoteWorkType.WorkType {
-									defaultWorkType = nil
-									break
-								}
-							}
-						}
-						// Input work type not found, using the last one we found from remote server
-						if defaultWorkType != nil {
-							localTask.WorkType = *defaultWorkType
-						}
-					}
-				} else {
-					setJobError(job, err)
-				}
-
-				// All done
-				setJobSuccess(job, "Found below task(s)")
-				taskParser = parser
-
-			} else {
-				setJobError(job, err)
-			}
-		} else {
+		_, err := common.GetCalendarEvents(weekOffset, subjectTemplate)
+		if err != nil {
 			setJobError(job, err)
+			return nil, nil, err
 		}
+
+		// if parser, err := common.NewTaskParser(inputFile); err == nil {
+		// 	for _, t := range parser.Tasks {
+		// 		if t != nil {
+		// 			tasks += t.Task + ","
+		// 		}
+		// 	}
+		// 	tasks = strings.TrimSuffix(tasks, ",")
+
+		// 	// Fetch needed details from remote
+
+		// 	if remoteTasks, err := client.FetchTasks(tasks); err == nil {
+		// 		// TODO: Optimise this? nested loops here
+		// 		for _, remoteTask := range *remoteTasks {
+		// 			// Also build the project IDs list
+		// 			projects += remoteTask.ProjectId + ","
+		// 			for _, localTask := range parser.Tasks {
+		// 				if localTask != nil && localTask.Task == remoteTask.LocalId {
+		// 					localTask.ProjectId = remoteTask.ProjectId
+		// 					localTask.Id = remoteTask.Id
+		// 					localTask.Title = remoteTask.Title
+		// 					// Truncate long title
+		// 					if len(localTask.Title) > 50 {
+		// 						localTask.Title = localTask.Title[:50]
+		// 					}
+		// 				}
+		// 			}
+		// 		}
+		// 		projects = strings.TrimSuffix(projects, ",")
+
+		// 		// Fetch work types, because they are setup per project
+		// 		if remoteWorkTypes, err := client.FetchProjectWorkTypes(projects); err == nil {
+		// 			// TODO: Optimise this? nested loops here
+		// 			for _, localTask := range parser.Tasks {
+		// 				var defaultWorkType *string
+		// 				// Walk all work types of the same project
+		// 				for _, remoteWorkType := range *remoteWorkTypes {
+		// 					if localTask != nil && localTask.ProjectId == remoteWorkType.ProjectId {
+		// 						localTask.WorkTypeId = remoteWorkType.WorkTypeId
+		// 						defaultWorkType = &remoteWorkType.WorkType
+		// 						// Finally found the match by work type name
+		// 						if localTask.WorkType == remoteWorkType.WorkType {
+		// 							defaultWorkType = nil
+		// 							break
+		// 						}
+		// 					}
+		// 				}
+		// 				// Input work type not found, using the last one we found from remote server
+		// 				if defaultWorkType != nil {
+		// 					localTask.WorkType = *defaultWorkType
+		// 				}
+		// 			}
+		// 		} else {
+		// 			setJobError(job, err)
+		// 		}
+
+		// 		// All done
+		// 		setJobSuccess(job, "Found below task(s)")
+		// 		taskParser = parser
+
+		// 	} else {
+		// 		setJobError(job, err)
+		// 	}
+		// } else {
+		// 	setJobError(job, err)
+		// }
 	}
 
-	// Render all jobs, until all done
-	time.Sleep(time.Millisecond * 100)
-	for pw.IsRenderInProgress() {
-	}
+	// // Render all jobs, until all done
+	// time.Sleep(time.Millisecond * 100)
+	// for pw.IsRenderInProgress() {
+	// }
 
-	if job.IsErrored() {
-		return nil, nil, errors.New("one or more steps throwing errors")
-	}
+	// if job.IsErrored() {
+	// 	return nil, nil, errors.New("one or more steps throwing errors")
+	// }
 
-	taskParser.DebugPrint()
-	result.tasks = taskParser.Tasks
+	// taskParser.DebugPrint()
+	// result.tasks = taskParser.Tasks
 
 	return result, client, nil
 }
@@ -209,7 +213,7 @@ func createTimeExecSteps(ctx CLIContext, prepResult *createTimePrepResult, clien
 
 func (c *timeCreateCmd) Run(ctx CLIContext) error {
 	// Prep checks
-	prepResult, client, err := createTimePrepSteps(ctx, c.InputFile)
+	prepResult, client, err := createTimePrepSteps(ctx, c.InputFile, c.WeekOffset, c.CalendarSubjectTemplate)
 	if err != nil {
 		return err
 	}
--- common/tasks_calendar.go
+++ common/tasks_calendar.go
@@ -3,6 +3,8 @@ package common
 import (
 	"context"
 	"fmt"
+	"regexp"
+	"strings"
 	"time"
 
 	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
@@ -21,7 +23,7 @@ type outLookCalendarEvent struct {
 	End        outLookCalendarEventTime `json:"end"`
 }
 
-func GetCalendarEvents(weekOffset int) (*[]TimeTaskInput, error) {
+func GetCalendarEvents(weekOffset int, subjectTemplate *string) (*[]TimeTaskInput, error) {
 	weekDays := GetWeekRange(time.Now(), weekOffset)
 	monday := weekDays[0]
 
@@ -35,8 +37,6 @@ func GetCalendarEvents(weekOffset int) (*[]TimeTaskInput, error) {
 		mondayUtc.Format(time.RFC3339),
 		nextMondayUtc.Format(time.RFC3339),
 	)
-
-	fmt.Println("Fetching calendar events with filter:", filter)
 
 	// Extra authenticity checks
 	filter += " and isDraft eq false"
@@ -71,11 +71,23 @@ func GetCalendarEvents(weekOffset int) (*[]TimeTaskInput, error) {
 
 	// Process each event
 	resultEvents := make([]TimeTaskInput, 0)
-	for _, event := range events {
-		if event.GetRecurrence() != nil {
+
+	// timeTaskMap := make(map[string]*TimeTaskInput)
+
+	for _, e := range events {
+		if e.GetRecurrence() != nil {
 			continue // TODO: skip recurrence events for now
 		}
+		// Get subject
+		if subject := e.GetSubject(); subject != nil {
+			code, desc, err := parseTaskFromSubject(*subject, *subjectTemplate)
+			if err != nil {
+				fmt.Printf("SKIPPED '%s': %v\n", *subject, err)
+				continue // Skip this event if parsing fails
+			}
 
+			fmt.Printf("Parsed task code: %s, description: %s\n", code, desc)
+		}
 	}
 
 	return &resultEvents, nil
@@ -110,5 +122,49 @@ func (e *outLookCalendarEvent) fromRemote(event models.Eventable) {
 				e.End.DateTime = parsedTime
 			}
 		}
+	}
+}
+
+// ParseTaskFromSubject parses a subject string using the provided template to extract task_code and task_desc
+// Template example: "[TT:{task_code}]{task_desc}"
+// Input example: "[TT:11238]Improve documentation"
+// Returns: task_code="11238", task_desc="Improve documentation", err=nil
+func parseTaskFromSubject(subject, template string) (taskCode, taskDesc string, err error) {
+	// Escape special regex characters in the template, but keep our placeholders
+	escapedTemplate := regexp.QuoteMeta(template)
+
+	// Replace our placeholders with regex capture groups
+	// {task_code} becomes (.+?) for non-greedy capture
+	// {task_desc} becomes (.+?) for non-greedy capture
+	regexPattern := escapedTemplate
+	regexPattern = strings.ReplaceAll(regexPattern, `\{task_code\}`, `(.+?)`)
+	regexPattern = strings.ReplaceAll(regexPattern, `\{task_desc\}`, `(.+)`) // greedy for task_desc as it's usually at the end
+
+	// Add start and end anchors to match the entire string
+	regexPattern = "^" + regexPattern + "$"
+
+	// Compile the regex
+	re, err := regexp.Compile(regexPattern)
+	if err != nil {
+		return "", "", err
+	}
+
+	// Find matches
+	matches := re.FindStringSubmatch(subject)
+	if len(matches) < 3 { // matches[0] is the full match, matches[1] and matches[2] are the groups
+		return "", "", fmt.Errorf("subject does not match template pattern")
+	}
+
+	// Determine which capture group corresponds to which placeholder
+	// We need to find the order of placeholders in the original template
+	taskCodeIndex := strings.Index(template, "{task_code}")
+	taskDescIndex := strings.Index(template, "{task_desc}")
+
+	if taskCodeIndex < taskDescIndex {
+		// task_code comes first
+		return strings.TrimSpace(matches[1]), strings.TrimSpace(matches[2]), nil
+	} else {
+		// task_desc comes first
+		return strings.TrimSpace(matches[2]), strings.TrimSpace(matches[1]), nil
 	}
 }
